df2656e54d64bfca11b8bb26a59212bcbd0f01c1
abel.agbachi <agbachiabel@gmail.com>
2022-03-18 12:39:08 +0100

    move .git folder to root path

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+// import Abelcomponent, { Leo } from './components/learningexamples/Abelcomponent';
+// import CounterButton from './components/counter/Counter';
+// import CalculateNum  from './components/Calc';
+// import PersonList from './components2/Team';
+// import Kounter from './components2/kounter/Kounter';
+//import DocumentationApp from './components3/documcomponents/DocumentationApp';
+import Deck from './cardapi/Deck';
+//import ListEmployeeComponent from './reactspringbootfrontend/ListEmployeeComponent';
+function App() {
+  return (
+    <>
+    
+      
+      <div className="container">
+      <Deck />
+
+      </div>
+     </>  
+    
+    
+    /*<div className="App">
+      <CounterButton by = {1}/>
+      <CounterButton by = {5} />
+      <CounterButton by = {10} />
+      
+    </div>
+    /*<div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <Abelcomponent></Abelcomponent>
+      <Leo></Leo>
+    </div>*/
+    
+
+
+  );
+}
+
+
+
+export default App;
